bca2faf3a10cf7abf20990ee4148480d4f408fe8
sureshYanamula <89737935+sureshYanamula@users.noreply.github.com>
2023-06-08 19:05:44 +0530

    AComp added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import React,{useState} from 'react';
-import { Switch, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Search from './pages/Search';
-import SearchAppBar from './components/SearchAppBar';
-import Bubbling from './components/Bubbling';
-import ThrottlingOne from './components/ThrottlingOne';
-import PropDrilling from './components/PropDrilling';
+import React, { useState } from "react";
+import { Switch, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Search from "./pages/Search";
+import SearchAppBar from "./components/SearchAppBar";
+import Bubbling from "./components/Bubbling";
+import PropDrilling from "./components/PropDrilling";
+import AComp from "./components/AComp";
 
 const App = () => {
-
-  
   return (
-    <div style={{textAlign:"center"}}>
+    <div style={{ textAlign: "center" }}>
       <SearchAppBar />
       {/* <h1>{count}</h1> */}
       <Switch>
-        <Route exact path='/' component={Home} />
-        <Route exact path='/search' component={Search} />
-        <Route exact path='/bubbling' component={Bubbling} />
-        <Route exact path='/throttling' component={ThrottlingOne} />
-        <Route exact path='/propdrilling' component={PropDrilling} />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/acomp" component={AComp} />
+        <Route exact path="/search" component={Search} />
+        <Route exact path="/bubbling" component={Bubbling} />
+        <Route exact path="/propdrilling" component={PropDrilling} />
       </Switch>
     </div>
   );
