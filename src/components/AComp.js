a9bbb3cf64349f543293a311f80ef6d85751d79d
sureshYanamula <89737935+sureshYanamula@users.noreply.github.com>
2023-06-09 21:46:45 +0530

    h6 change

--- src/components/AComp.js
+++ src/components/AComp.js
@@ -5,7 +5,7 @@ function AComp() {
     <div>
       <p>Change Made</p>
       <h4>Another Change Made</h4>
-      <h3> Change 3</h3>
+      <h6> Change 3</h6>
       <p>feature1 change</p>
     </div>
   );
