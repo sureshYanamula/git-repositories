aa2d5874ae872bfe44f28dfe25409f119d9c341c
sureshYanamula <ysureshy.1994@gmail.com>
2023-07-17 22:02:27 +0530

    redux tutorials

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
-import { combineReducers } from 'redux';
-import { userReducer } from './userReducer';
-import { searchReducer } from './searchReducer';
+import { combineReducers } from "redux";
+import { userReducer } from "./userReducer";
+import { searchReducer } from "./searchReducer";
+import { typicodeReduxStateReducer } from "./typicodeReduxStateReducer";
 
 const rootReducer = combineReducers({
   user: userReducer,
   search: searchReducer,
+  typicodeReduxStateReducer: typicodeReduxStateReducer,
 });
 
 export default rootReducer;
---
+++ src/reducers/typicodeReduxStateReducer.js
@@ -0,0 +1,15 @@
+const initialState = { loading: true, error: "", userData: [] };
+
+export const typicodeReduxStateReducer = (state = initialState, action) => {
+  console.log(action, "action");
+  switch (action.type) {
+    case "TYPI_USER_PENDING":
+      return { loading: true, error: "", userData: [] };
+    case "TYPI_USER_SUCCESS":
+      return { loading: false, error: "", userData: action.payload };
+    case "TYPI_USER_FAILURE":
+      return { loading: false, error: "something went wrong", userData: [] };
+    default:
+      return state;
+  }
+};
